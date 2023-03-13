0a4d5136cfa37f07c21f940c80d7d699242bb5fd
ericazlin <ericazlin@gmail.com>
2023-03-12 23:21:49 -0400

    okay this is like the beginning

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,74 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1>i love shkola</h1>
+    <h1 v-if="number">{{ text }}</h1>
+
+    <ul>
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </ul>
+
+
+    <button @click="authState" v-if="loggedIn">Log Out yo</button>
+    <button @click="authState" v-else>Log In yo</button>
+
+    <br>
+    <input type="text" placeholder="im going down" v-model="message">
+    <p>{{ message }}</p>
+
+    <h1>{{ time }}</h1>
+
+    <Button @button-click="frigginThing">i am a button</Button>
+    <p v-if="loggedIn">{{ poop }}</p>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<style scoped></style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+import Button from './components/Button.vue'
+
+export default {
+  data() {
+    return {
+      text: "ochen da",
+      number: true,
+
+      items: ["I", "am", "an", "array", "being", "iterated"],
+      loggedIn: false,
+
+      message: "",
+      poop: "",
+    };
+  },
+
+  components: {
+    Button,
+  },
+
+  computed: {
+    time: function () {
+      const time = new Date;
+      return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+    }
+  },
+  methods: {
+    authState: function () {
+      if (this.loggedIn === false) {
+        this.loggedIn = true;
+      }
+      else {
+        this.loggedIn = false;
+      }
+    },
+    frigginThing: function () {
+      if (this.poop === "") {
+        this.poop = "poop"
+      }
+      else {
+        this.poop = ""
+      }
+    },
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+};
+</script>
---
+++ vue-project/src/components/Button.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+<button @click="$emit('button-click')"><slot></slot></button>
+</template>
+
+<script>
+    export default {
+        name: "Button",
+    }
+</script>
+
+<style scoped>
+    button {
+        background-color: green;
+        width: 150px;
+        height: 300px;
+    }
+</style>
