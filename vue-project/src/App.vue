66feb46eb1755d00c56f18c323bae4e0be615801
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-03-20 13:13:31 -0400

    added some products ALSO A+ ON PHYSICS MC TEST BROO

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -52,28 +52,34 @@ export default {
 
       message: "",
       poop: "poop",
-
       destinations: [
         {
-          title: "i am an assortment of words",
+          title: "donuts",
           description:
-            "not nessecarily with any importance or meaning behind them",
+            "$",
           image:
-            "https://images.unsplash.com/photo-1514496959998-c01c40915c5f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+            "https://cdn.discordapp.com/attachments/829231612012527646/1054988265092026408/donuts.jpg",
         },
         {
-          title: "i dont know what else to write :(",
+          title: "chips",
           description:
-            "i only need one more after this so i can use dynamic props",
+            "$",
           image:
-            "https://images.unsplash.com/photo-1678398176993-dc9cb4df6763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIyfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60 ",
+            `https://cdn.discordapp.com/attachments/970059739318849597/1049201473617141872/chips.jpg`,
         },
         {
-          title: "unsplash is pretty cool for free photos",
+          title: "minced meat",
           description:
-            "i like snow and mountains and nature and stuff like that",
+            "$",
           image:
-            "https://images.unsplash.com/photo-1678497180488-d0f1cf12e61c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfGJvOGpRS1RhRTBZfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+            "https://cdn.discordapp.com/attachments/970059739318849597/1049201474149822515/minced_meat.jpg",
+        },
+        {
+          title: "whole bread",
+          description:
+            "$",
+          image:
+            "https://cdn.discordapp.com/attachments/970059739318849597/1049201474380505128/whole_bread.jpg",
         },
       ],
     };
