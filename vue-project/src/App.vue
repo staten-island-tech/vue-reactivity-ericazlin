394ad0362d01a7875b47b16e6bac3ae8d0424a1c
ericazlin <113201249+ericazlin@users.noreply.github.com>
2023-03-23 13:14:11 -0400

    half day tmrr

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,30 +3,13 @@
     <div class="container">
       <div class="texts">
         <div>
-          <h1>i love shkola</h1>
+          <h1>buy yo groceries here</h1>
           <h1 v-if="number">{{ text }}</h1>
 
           <ul>
             <li v-for="item in items" :key="item">{{ item }}</li>
           </ul>
         </div>
-
-        <div>
-          <button @click="authState" v-if="loggedIn">Log Out yo</button>
-          <button @click="authState" v-else>Log In yo</button>
-        </div>
-
-        <div>
-          <input type="text" placeholder="type smth here omg" v-model="message" />
-          <p>{{ message }}</p>
-        </div>
-
-        <div>
-          <h1>{{ time }}</h1>
-        </div>
-
-        <Button @button-click="frigginThing">i am a button</Button>
-        <p v-if="loggedIn">{{ poop }}</p>
       </div>
 
       <div class="cards">
@@ -45,9 +28,7 @@ import {products} from '../src/products';
 export default {
   data() {
     return {
-      text: "ochen da",
-      number: true,
-      items: ["I", "am", "an", "array", "being", "iterated"],
+   
 loggedIn: false,
 products,
 
@@ -61,28 +42,6 @@ poop: "poop",
     Card,
   },
 
-  computed: {
-    time: function () {
-      const time = new Date();
-      return `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`;
-    },
-  },
-  methods: {
-    authState: function () {
-      if (this.loggedIn === false) {
-        this.loggedIn = true;
-      } else {
-        this.loggedIn = false;
-      }
-    },
-    frigginThing: function () {
-      if (this.poop === "") {
-        this.poop = "poop";
-      } else {
-        this.poop = "";
-      }
-    },
-  },
 };
 </script>
 
